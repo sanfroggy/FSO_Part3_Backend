ebcfa28b2cd6cfdac5ecc275426adb01430c423a
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-20 00:08:25 +0300

    Edited part 3.13

--- index.js
+++ index.js
@@ -16,30 +16,6 @@ morgan.token('body', (req) => {
     }
 })
 
-//Creating an array containing the data of 4 contacts.
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "0100 23569034"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-    }
-]
-
 /*Creating a function to return a random number between
 0 and 10000 */
 const getRandomId = () => {
